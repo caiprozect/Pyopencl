0dc62c5470c2b84274b5c4c6b03b6ef932f084e4
unknown <caiprozect@gmail.com>
2018-04-02 22:26:10 +0900

    Sandbox1 Complete

--- sandbox.py
+++ sandbox.py
@@ -2,24 +2,28 @@ import pyopencl as cl
 import numpy as np 
 from time import time
 import os
-from collections import Counter
+from itertools import product
+from functools import reduce
+from operator import mul
 
+os.environ["PYOPENCL_CTX"]=''
 CPU_SIDE_INT = np.int32 #Change this according to architecture
 
-def main():
-	#fname = "../data/hg38.chroms/chr1.fa"
-	fname = "E.ColiGenome.txt" #Toy example
-	K = 1
-	f = open(fname, 'rb')
-	#f.readline()
+def genSeq(file):
+	print("Generate seq from {}".format(file))
+	f = open(file, 'rb')
+	f.readline()
 	data = f.read().upper().splitlines()
 	f.close()
-	data = (''.encode('utf-8')).join(data)
-	#print(Counter(data))
-	#print(len(data))
-	h_seq = np.frombuffer(data, dtype=np.uint8)
-	h_seq = h_seq.astype(CPU_SIDE_INT)
-	h_seq = np.concatenate((np.zeros(4**K+2).astype(CPU_SIDE_INT), h_seq))
+	data = ("".encode('utf-8')).join(data)	
+	seq = np.frombuffer(data, dtype=np.uint8).astype(CPU_SIDE_INT)
+	print("Seq for {} has been generated".format(file))
+	return seq
+
+def kMerCount(file, nK):
+	K = nK
+	h_seq = genSeq(file)
+	h_seq = np.concatenate((np.zeros(2+4+4**K).astype(CPU_SIDE_INT), h_seq))
 
 	kernelsource = '''
 	__kernel void mapToNumb(
@@ -31,23 +35,27 @@ def main():
 	)
 	{
 		int gid = get_global_id(0);
-		int idx = gid * M + numbKmer + 2;
+		int idx = gid * M + numbKmer + 2 + 4;
 		int i, letter;
 
-		if(idx < N*M + numbKmer + 2) {
+		if(idx < N*M + numbKmer + 2 + 4) {
 			for(i=0; i < M; i++) {
 				letter = seq[idx+i];
 				if(letter == 65) {
 					numb_seq[idx+i] = 0;
+					atomic_inc(&numb_seq[2]);
 				} else {
 				if(letter == 67) {
 					numb_seq[idx+i] = 1;
+					atomic_inc(&numb_seq[3]);
 				} else {
 				if(letter == 71) {
 					numb_seq[idx+i] = 2;
+					atomic_inc(&numb_seq[4]);
 				} else {
 				if(letter == 84) {
 					numb_seq[idx+i] = 3;
+					atomic_inc(&numb_seq[5]);
 				} else {
 				if(letter == 78) {
 					numb_seq[idx+i] = (-1) * numbKmer;
@@ -66,18 +74,17 @@ def main():
 		const int M,
 		const int nK,
 		const int numbKmer,
-		__global int* numb_seq,
-		__global int* freq_seq
+		__global int* numb_seq
 	) {
 		int gid = get_global_id(0);
-		int idx = gid * M + numbKmer + 2;
+		int idx = gid * M + numbKmer + 2 + 4;
 		int i, numb;
 		int k, p, loc_idx, ptn_idx;
 		int dgt;
 		for(i=0; i < M; i++) {
 			ptn_idx = 0;
 			loc_idx = idx + i;
-			if(loc_idx <= (N*M + numbKmer + 2 - nK)) {
+			if(loc_idx <= (N*M + numbKmer + 2 + 4 - nK)) {
 				for(k=0; k < nK; k++) {
 					dgt = 1;
 					numb = numb_seq[loc_idx + k];
@@ -86,14 +93,14 @@ def main():
 					}
 					ptn_idx += dgt * numb;
 				}
-				ptn_idx += 2;
+				ptn_idx += 2 + 4;
 				if(ptn_idx >= 0) {
-					atomic_inc(&freq_seq[ptn_idx]);
+					atomic_inc(&numb_seq[ptn_idx]);
 				} else {
 				if(ptn_idx < -1000000) {
-					atomic_inc(&freq_seq[0]);
+					atomic_inc(&numb_seq[0]);
 				} else{
-					atomic_inc(&freq_seq[1]);
+					atomic_inc(&numb_seq[1]);
 				}
 				}
 			}
@@ -112,7 +119,7 @@ def main():
 	numbGroups = work_group_size
 	numbItems = work_item_size
 
-	seqLen = np.size(h_seq) - 4**K - 2
+	seqLen = np.size(h_seq) - 4**K - 2 - 4
 	q, r = divmod(seqLen, numbGroups*numbItems)
 	q = q + 1
 	h_seq = np.concatenate((h_seq, np.repeat(78, numbGroups*numbItems-r).astype(CPU_SIDE_INT)))
@@ -123,12 +130,12 @@ def main():
 	queue = cl.CommandQueue(context)
 	program = cl.Program(context, kernelsource).build()
 	mapToNumb = program.mapToNumb
-	mapToNumb.set_scalar_arg_dtypes([CPU_SIDE_INT, CPU_SIDE_INT, CPU_SIDE_INT, None, None])
+	mapToNumb.set_scalar_arg_dtypes([np.int32, np.int32, np.int32, None, None])
 	freqTab = program.freqTab
-	freqTab.set_scalar_arg_dtypes([CPU_SIDE_INT, CPU_SIDE_INT, CPU_SIDE_INT, CPU_SIDE_INT, None, None])
+	freqTab.set_scalar_arg_dtypes([np.int32, np.int32, np.int32, np.int32, None])
 
 	d_seq = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf = h_seq)
-	d_numb_seq = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, h_numb_seq.nbytes)
+	d_numb_seq = cl.Buffer(context, cl.mem_flags.READ_WRITE, h_numb_seq.nbytes)
 	cl.enqueue_fill_buffer(queue, d_numb_seq, np.zeros(1).astype(np.int), 0, h_numb_seq.nbytes)
 
 	N = numbGroups*numbItems
@@ -141,27 +148,51 @@ def main():
 	
 	queue.finish()
 
-	cl.enqueue_copy(queue, h_numb_seq, d_numb_seq)
-	d_seq.release()
-	d_numb_seq.release()
-	#print(Counter(h_numb_seq))
-
-	d_numb_seq = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf = h_numb_seq)
-	h_freq_seq = np.zeros(np.size(h_seq)).astype(CPU_SIDE_INT)
-	d_freq_seq = cl.Buffer(context, cl.mem_flags.WRITE_ONLY, h_freq_seq.nbytes)
-	cl.enqueue_fill_buffer(queue, d_freq_seq, np.zeros(1).astype(np.int), 0, h_freq_seq.nbytes)
-
-	freqTab(queue, globalsize, None, N, M, K, numbKmer, d_numb_seq, d_freq_seq)
-
-	print("check point")
+	freqTab(queue, globalsize, None, N, M, K, numbKmer, d_numb_seq)
 
 	queue.finish()
 
-	cl.enqueue_copy(queue, h_freq_seq, d_freq_seq)
+	cl.enqueue_copy(queue, h_numb_seq, d_numb_seq)
 
-	print("Mapping Done")
-	print(h_freq_seq[:numbKmer+2])
-	#print(Counter(data))
+	print("Counting Done")
+
+	print(h_numb_seq[:numbKmer+2+4])
+	assert(h_numb_seq[0] == 0), "File contains unknown nucleotide characters"
+
+	return h_numb_seq[2:numbKmer+2+4]
+
+def processOrganChromDict(organChromDict, nK, outfile):
+	lNucls = ["A", "C", "G", "T"]
+	lKmer = product(range(4), repeat=nK)
+	with open(outfile, 'w') as f:	
+		seqNameList = organChromDict.keys()
+		for seqName in seqNameList:
+			chromNumbs = organChromDict[seqName]
+			chromFileList = ["../data/{}.chroms/chr{}.fa".format(seqName, numb) for numb in chromNumbs]
+			freqs = np.array([kMerCount(file, K) for file in chromFileList])
+			freqs = np.sum(freqs, axis=0)
+			monoCnts = freqs[:4]
+			monoFreqs = monoCnts / np.sum(monoCnts)
+			polyCnts = freqs[4:4+4**nK]
+			polyFreqs = polyCnts / np.sum(polyCnts)
+			polyExps = [reduce(mul, kmer, 1) in lKmer]
+			flds = "{:6}{:16}{:16}{:16}"
+			f.write("{} chromosomes statistics".format(seqName))
+			f.write(flds.format("K-mer", "Counts", "Frequencies", "Expectations"))
+			for i in range(4):
+				f.write(flds.format(lNucls[i], monoCnts[i], monoFreqs[i], ""))
+			for i in range(4**nK):
+				kmer = lKmer[i]
+				kmer = reduce((lambda x,y: x+lNucls[y]), kmer, "")
+				f.write(flds.format(kmer, polyCnts[i], polyFreqs[i], polyExps[i]))
+			f.write("\n")
+
+def main():
+	dictChromCat = {'hg38': ([str(i) for i in range(1,23)]+['X','Y']), 'galGal3': ([str(i) for i in range(1,29)]+['32','W','Z']),
+					'dm3': ['2R', '2L', '3R', '3L', '4', 'X'], 'ce10': ['I', 'II', 'III', 'IV', 'V', 'X']}
+	K = 2
+	outFile = "Mission1_Opencl.txt"
+	processOrganChromDict(dictChromCat, K, outFile)
 
 if __name__=="__main__":
 	rtime = time()
